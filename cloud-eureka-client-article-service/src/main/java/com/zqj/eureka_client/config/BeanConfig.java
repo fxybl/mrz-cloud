2a0b12f20f61ebcb0b5b49c748343529fd1c5d4f
zhaoquanjie <502527463@qq.com>
2019-12-31 18:18:23 +0800

    eureka服务消费者

--- cloud-eureka-client-article-service/src/main/java/com/zqj/eureka_client/config/BeanConfig.java
+++ cloud-eureka-client-article-service/src/main/java/com/zqj/eureka_client/config/BeanConfig.java
@@ -14,7 +14,9 @@ import org.springframework.web.client.RestTemplate;
 public class BeanConfig {
 
     @Bean
-    //这个注解会自动构建LoadBalancerClient接口的实现类并注册到spring容器中
+    //这个注解会自动构建LoadBalancerClient接口的实现类并注册到spring容器中,
+    //启用ribbon负载均衡,根据负载均衡策略请求集群中的一个服务实例
+    //ribbon会自动将服务名字组装成对应的IP地址
     @LoadBalanced
     public RestTemplate getRestTemplate(){
         return new RestTemplate();
