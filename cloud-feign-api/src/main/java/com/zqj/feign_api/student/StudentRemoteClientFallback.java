ef4ad1b0a5dee3f7c401694a5cbea400a2d1c230
zhaoquanjie <502527463@qq.com>
2020-01-02 16:05:29 +0800

    feign-api

--- cloud-feign-api/src/main/java/com/zqj/feign_api/student/StudentRemoteClientFallback.java
+++ cloud-feign-api/src/main/java/com/zqj/feign_api/student/StudentRemoteClientFallback.java
@@ -1,11 +1,15 @@
 package com.zqj.feign_api.student;
 
+import org.springframework.stereotype.Component;
+
 /**
  *
  * hystrix调用失败后的处理
  * @author zqj
  * @create 2020-01-02 15:46
  */
+
+@Component
 public class StudentRemoteClientFallback implements StudentRemoteClient {
 
     @Override
