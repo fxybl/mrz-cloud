14ecb9cb7261284fdf3b7bb601397a6847307d10
zhaoquanjie <502527463@qq.com>
2020-01-02 16:56:34 +0800

    gateway

---
+++ cloud-gateway/src/main/java/com/zqj/gateway/Main.java
@@ -0,0 +1,14 @@
+package com.zqj.gateway;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+
+/**
+ * @author zqj
+ * @create 2020-01-02 16:53
+ */
+
+@SpringBootApplication
+@EnableEurekaClient
+public class Main {
+}
