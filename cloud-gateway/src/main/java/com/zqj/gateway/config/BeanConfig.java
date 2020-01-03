ea4570dd1fce2e0dc4032892d139b9d541fb08ed
zhaoquanjie <502527463@qq.com>
2020-01-03 16:26:47 +0800

    gateway

--- cloud-gateway/src/main/java/com/zqj/gateway/config/BeanConfig.java
+++ cloud-gateway/src/main/java/com/zqj/gateway/config/BeanConfig.java
@@ -7,6 +7,7 @@ import reactor.core.publisher.Mono;
 
 /**
  * 限流bean
+ *
  * @author zqj
  * @create 2020-01-03 16:14
  */
@@ -16,15 +17,15 @@ public class BeanConfig {
 
     //使用IP进行限流
     @Bean
-    public KeyResolver ipKeyResolver(){
-     return exchange ->
-             //IP作为唯一标识
-         Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
+    public KeyResolver ipKeyResolver() {
+        return exchange ->
+                //IP作为唯一标识
+                Mono.just(exchange.getRequest().getRemoteAddress().getHostName());
     }
 
     //使用用户进行限流
     @Bean
-    public KeyResolver userKeyResolver(){
+    public KeyResolver userKeyResolver() {
         return exchange ->
                 //用户ID作为唯一标识
                 Mono.just(exchange.getRequest().getQueryParams().getFirst("userId"));
@@ -32,7 +33,7 @@ public class BeanConfig {
 
     //使用请求地址进行限流
     @Bean
-    public KeyResolver uriKeyResolver(){
+    public KeyResolver uriKeyResolver() {
         return exchange ->
                 //地址作为唯一标识
                 Mono.just(exchange.getRequest().getPath().value());
