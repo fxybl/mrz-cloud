0f0216bcd720d7f3ce12c4f5bdb3faac4f772310
zhaoquanjie <502527463@qq.com>
2020-01-07 18:10:25 +0800

    eureka服务消费者

--- cloud-eureka-client-article-service/src/main/java/com/zqj/eureka_client/controller/StudentController.java
+++ cloud-eureka-client-article-service/src/main/java/com/zqj/eureka_client/controller/StudentController.java
@@ -3,6 +3,7 @@ package com.zqj.eureka_client.controller;
 import brave.Tracer;
 import com.zqj.feign_api.student.Student;
 import com.zqj.feign_api.student.StudentRemoteClient;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,17 +14,18 @@ import org.springframework.web.bind.annotation.RestController;
  */
 
 @RestController
+@Slf4j
 public class StudentController {
 
     @Autowired
     private StudentRemoteClient studentRemoteClient;
 
-    @Autowired
-    Tracer tracer;
+    //@Autowired
+    //Tracer tracer;
 
     @GetMapping("/student/info")
     public Student getStudentInfo(String name){
-        tracer.currentSpan().tag("zqj","666");
+        //tracer.currentSpan().tag("zqj","666");
         return studentRemoteClient.getStudent(name);
     }
 }
