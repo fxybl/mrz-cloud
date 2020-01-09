00c315d00c836ad981b69330b42bbac9ff21ec75
zhaoquanjie <502527463@qq.com>
2020-01-09 16:35:32 +0800

    分布式事务task

--- cloud-transaction-task/src/main/java/com/zqj/transaction_task/Main.java
+++ cloud-transaction-task/src/main/java/com/zqj/transaction_task/Main.java
@@ -2,6 +2,8 @@ package com.zqj.transaction_task;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 /**
  * @author zqj
@@ -9,6 +11,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  */
 
 @SpringBootApplication
+@EnableDiscoveryClient
+@EnableFeignClients
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
