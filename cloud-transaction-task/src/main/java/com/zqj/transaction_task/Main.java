5484c29d84dfb1b693d50fb679eb48a266305e44
zhaoquanjie <502527463@qq.com>
2020-01-09 16:29:36 +0800

    分布式事务task

---
+++ cloud-transaction-task/src/main/java/com/zqj/transaction_task/Main.java
@@ -0,0 +1,16 @@
+package com.zqj.transaction_task;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zqj
+ * @create 2020-01-09 16:25
+ */
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
