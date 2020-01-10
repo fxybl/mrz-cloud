950dfb84dc08bffd287fce38afb0d7711cc37e4c
zhaoquanjie <502527463@qq.com>
2020-01-10 14:57:46 +0800

    事务-库存

---
+++ cloud-transaction-stock/src/main/resources/stock.sql
@@ -0,0 +1,32 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : 192.168.0.18
+Source Server Version : 50724
+Source Host           : 192.168.0.18:3306
+Source Database       : stock
+
+Target Server Type    : MYSQL
+Target Server Version : 50724
+File Encoding         : 65001
+
+Date: 2020-01-10 14:57:17
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for stock
+-- ----------------------------
+DROP TABLE IF EXISTS `stock`;
+CREATE TABLE `stock` (
+  `id` bigint(20) NOT NULL,
+  `goods_id` bigint(20) DEFAULT NULL COMMENT '商品ID',
+  `count` int(11) DEFAULT NULL COMMENT '库存数量',
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- ----------------------------
+-- Records of stock
+-- ----------------------------
+INSERT INTO `stock` VALUES ('1', '10086', '10');
