18b1c34a1d800c95d99a1ec2a88f304b67d9e050
zhaoquanjie <502527463@qq.com>
2020-01-14 14:02:45 +0800

    分布式事务客户端

--- cloud-transaction-client/src/main/java/com/zqj/transaction/message/TransactionRemoteClient.java
+++ cloud-transaction-client/src/main/java/com/zqj/transaction/message/TransactionRemoteClient.java
@@ -15,35 +15,83 @@ import java.util.List;
 @FeignClient(value = "cloud-transaction-service", path = "/message", fallback = TransactionRemoteClientHystrix.class)
 public interface TransactionRemoteClient {
 
-    //发送消息，只保存到数据库表中，由其他的线程执行发送
+    /**
+     * 发送消息，只保存到数据库表中，由其他的线程执行发送
+     * @param message
+     * @return
+     */
     @PostMapping("/send")
     boolean sendMessage(@RequestBody TransactionMessage message);
 
-    //批量发送消息
+
+
+    /**
+     * 批量发送消息
+     * @param messages
+     * @return
+     */
     @PostMapping("/sends")
     boolean sendMessages(@RequestBody List<TransactionMessage> messages);
 
-    //累加发送次数
+
+
+    /**
+     * 累加发送次数
+     * @param messageId
+     * @param sendDate
+     * @return
+     */
     @PostMapping("/incrSendCount")
     boolean incrSendCount(@RequestParam("messageId") Long messageId, @RequestParam("sendDate") String sendDate);
 
-    //确定消息死亡
+
+
+    /**
+     * 确定消息死亡
+     * @param messageId
+     * @return
+     */
     @PostMapping("/confirmDieMessage")
     boolean confirmDieMessage(@RequestParam("messageId") Long messageId);
 
-    //确定消息被消费
+
+
+    /**
+     * 确定消息被消费
+     * @param messageId
+     * @param consumerSystem
+     * @return
+     */
     @PostMapping("/confirmCustomerMessage")
     boolean confirmCustomerMessage(@RequestParam("messageId") Long messageId,@RequestParam("consumerSystem") String consumerSystem);
 
-    //查询最早的没有被消费的消息
+
+
+    /**
+     * 查询最早的没有被消费的消息
+     * @param limit
+     * @return
+     */
     @GetMapping("/queryByWaitingMessage")
     List<TransactionMessage> queryByWaitingMessage(@RequestParam("limit") int limit);
 
-    //重新发送已经死亡的消息
+
+
+    /**
+     * 重新发送已经死亡的消息
+     * @return
+     */
     @PostMapping("/retrySendDieMessage")
     boolean retrySendDieMessage();
 
-    //根据状态分页查询消息
+
+    /**
+     * 根据状态分页查询消息
+     * @param status
+     * @param currentPage
+     * @param pageSize
+     * @return
+     */
     @GetMapping("queryMessagePage")
     List<TransactionMessage> queryMessagePage(@RequestParam("status") int status, @RequestParam("currentPage") int currentPage, @RequestParam("pageSize") int pageSize);
 
