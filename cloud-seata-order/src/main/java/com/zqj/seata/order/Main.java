f6eb7f94dae0f54baee3e3c3f3c5e03532b9b7b0
zhaoquanjie <502527463@qq.com>
2020-01-17 13:59:51 +0800

    seata分布式事务order服务端

--- cloud-seata-order/src/main/java/com/zqj/seata/order/Main.java
+++ cloud-seata-order/src/main/java/com/zqj/seata/order/Main.java
@@ -1,7 +1,9 @@
 package com.zqj.seata.order;
 
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
@@ -9,10 +11,11 @@ import org.springframework.cloud.openfeign.EnableFeignClients;
  * @author zqj
  * @create 2020-01-16 10:19
  */
-
-@SpringBootApplication
+//去除spring的datasource的自动初始化，使用自定义的
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
 @EnableFeignClients(basePackages = "com.zqj.seata.api.remote")
 @EnableDiscoveryClient
+@MapperScan("com.zqj.seata.order.mapper")
 public class Main {
     public static void main(String[] args) {
         SpringApplication.run(Main.class,args);
