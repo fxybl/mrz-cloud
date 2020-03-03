164a6d1e1c8d777555e756cbc3b85861e06d0d6f
zqj <jingjingqidao@vip.qq.com>
2020-03-03 15:10:07 +0800

    自定义apollo

---
+++ cloud-my-apollo/src/main/java/com/zqj/my/apollo/Main.java
@@ -0,0 +1,16 @@
+package com.zqj.my.apollo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author zqj
+ * @create 2020-03-03 14:16
+ */
+
+@SpringBootApplication
+public class Main {
+    public static void main(String[] args) {
+        SpringApplication.run(Main.class,args);
+    }
+}
